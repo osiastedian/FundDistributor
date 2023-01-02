50bedd39339fd6b36f1ea46f09b9024fad9badc8
Ted Ian Osias <osiastedian@gmail.com>
2023-01-02 19:35:11 +0800

    add tanembaum

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,23 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {},
+    rollux: {
+      url: "https://testnet.rollux.com:2814/",
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+      chainId: 2814,
+    },
+    tanembaum: {
+      url: "https://rpc.tanenbaum.io/",
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+      chainId: 5700,
+    },
+  },
 };
 
 export default config;
